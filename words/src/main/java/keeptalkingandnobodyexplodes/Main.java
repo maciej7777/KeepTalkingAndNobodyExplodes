54213b6c1ac2105bf5919512823dbc78c0d2a89c
Maciej Uniejewski <maciej7777@macs-mbp.mynet>
2021-11-01 17:07:22 +0100

    Create new repository

---
+++ words/src/main/java/keeptalkingandnobodyexplodes/Main.java
@@ -0,0 +1,15 @@
+package keeptalkingandnobodyexplodes;
+
+import keeptalkingandnobodyexplodes.words.WordsSolver;
+
+public class Main {
+
+    public static void main(String[] args) {
+        WordsSolver solver = new WordsSolver();
+
+        while (true) {
+
+        }
+        //System.out.println(solver.solve(args));
+    }
+}
