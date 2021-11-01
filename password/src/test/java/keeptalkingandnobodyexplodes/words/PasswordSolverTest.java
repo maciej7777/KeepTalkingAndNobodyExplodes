9b257d0320258c5d6bc84598efc52ae3bdc599ce
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2021-11-01 21:57:18 +0100

    refactor code

--- password/src/test/java/keeptalkingandnobodyexplodes/words/PasswordSolverTest.java
+++ password/src/test/java/keeptalkingandnobodyexplodes/words/PasswordSolverTest.java
@@ -9,16 +9,17 @@ import static org.testng.Assert.assertEquals;
 
 public class PasswordSolverTest {
 
-
     @DataProvider
     public static Object[][] inputQueryAndExpectedOutput() {
         return new Object[][]{
                 {"1:ce 2:ae", Set.of("cecha")},
+                {"2:ae 1:ce", Set.of("cecha")},
+                {"1:ce 3:cz", Set.of("cecha")},
                 {"1:wx 2:ez, 3:ł", Set.of("wełna")},
                 {"1:a 2:pl", Set.of("apacz", "alarm")},
+                {"1:x 2:z", Set.of()},
         };
     }
-
 
     @Test(dataProvider = "inputQueryAndExpectedOutput")
     public void testSolve(String input, Set<String> expectedOutput) {
